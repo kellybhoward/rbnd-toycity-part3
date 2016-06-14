8267bfbcc0af3029bdb8e527b2e8e2dd97afb857
Kelly Howard <kelly.howard@knowlabs.com>
2016-06-14 15:47:23 -0700

    Added in_stock methods for Product checking

--- lib/product.rb
+++ lib/product.rb
@@ -1,14 +1,38 @@
 class Product
-    attr_reader :title
+    attr_reader :title, :price, :stock
     @@products = []
+    @@in_stock = []
 
     def initialize(options={})
         @title = options[:title]
+        @price = options[:price]
+        @stock = options[:stock]
         add_to_products
     end
 
     def self.all
         @@products
+    end
+
+    def self.in_stock
+        @@products.each do |product|
+            if product.in_stock?
+                @@in_stock << product
+            end
+        end
+        return @@in_stock
+    end
+
+    def in_stock?
+        @stock > 0
+    end
+
+    def self.find_by_title(title)
+        @@products.each do |p|
+            if p.title == title
+                return p
+            end
+        end
     end
 
     private
